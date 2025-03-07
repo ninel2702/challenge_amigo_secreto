090c7adb9c4ec65fc862def94d4668d9db2f728e
ninel2702 <ninel.2702@gmail.com>
2025-03-07 11:36:31 -0600

    Capturar el valor del campo de entrada

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 //declarar la variable de los amigos primer commit del git 
 let amigos=[];
 //esta es la funcion de agregar amigo 
-function agregarAmigo(){}
+function agregarAmigo(){
+  //Capturar el valor del campo de entrada
+let nombreAmigo=(document.getElementById('amigo').value);
+}
