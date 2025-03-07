24cc1bac6760dc8336fc5234d46456a32afe4e87
ninel2702 <ninel.2702@gmail.com>
2025-03-07 11:46:49 -0600

    Actualizar el array de amigos

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ let amigos=[];
 function agregarAmigo(){
   //Capturar el valor del campo de entrada
 let nombreAmigo=(document.getElementById('amigo').value);
+  //esta es para validar cuando el campo este vacio
   if (nombreAmigo === ""){
     alert("por favor ingresa un nombre valido");
     return;
 }
+  amigos.push(nombreAmigo);
+console.log(nombreAmigo);
 }
